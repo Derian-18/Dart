509d365647307ffdf13391a262ec635cd46d696c
Derian-18 <derian.jimenezramirez@cesunbc.edu.mx>
2026-09-24 16:51:25 -0700

    feat: POO y promesas

---
+++ sesion06A.dart
@@ -0,0 +1,18 @@
+// Sesion 06 A
+
+void main() {
+  print("Inicio del programa");
+
+  // Uso correcto del Future con .then()
+  gethttpRequest("http://www.cesun.edu.mx").then((value) {
+    print(value);
+  });
+
+  print("Fin del programa");
+}
+
+Future<String> gethttpRequest(String url) {
+  return Future.delayed(Duration(seconds: 1), () {
+    return "Promesa asincrona finalizada";
+  });
+}
